7c2bf4cb03795cb3ce11a9a99f26a2325950cb04
HugoMassaria <hugo@marjory.co>
2024-02-05 10:28:06 +0100

    Database & Front & Back

--- script.sh
+++ script.sh
@@ -5,9 +5,38 @@ echo "Creating gitlab server ..."
 doctl compute droplet create debian-gitlab-server --droplet-agent=true --enable-monitoring --image ubuntu-20-04-x64 --no-header --region LON1 --size s-4vcpu-8gb --ssh-keys de:8e:e4:13:b9:11:56:dd:05:f1:ce:1c:87:82:de:f7 --tag-name gitlab --wait
 ## Assign reserved-ip to debian-gitlab-server
 echo "Assign reserved ip to gitlab server ..."
-ID=$(doctl compute droplet get debian-gitlab-server | awk 'NR==2{print $1}')
-doctl compute reserved-ip-action assign 159.65.211.103 $ID
+ID_GITLAB=$(doctl compute droplet get debian-gitlab-server | awk 'NR==2{print $1}')
+doctl compute reserved-ip-action assign 159.65.211.103 $ID_GITLAB
+sleep 10s
 ## Deploy Gitlab server
 echo "Deploy gitlab server ..."
 ansible-playbook -i ./ansible/hosts.ini ./ansible/gitlab/deploy_server.yml
 ansible-playbook -i ./ansible/hosts.ini ./ansible/configure_firewall.yml
+
+# Server
+
+## Create debian-server
+echo "Creating server ..."
+doctl compute droplet create debian-server --droplet-agent=true --enable-monitoring --image ubuntu-20-04-x64 --no-header --region LON1 --size s-4vcpu-8gb --ssh-keys de:8e:e4:13:b9:11:56:dd:05:f1:ce:1c:87:82:de:f7 --tag-name server --wait
+## Assign reserved-ip to server
+echo "Assign reserved ip to server ..."
+ID_SERVER=$(doctl compute droplet get debian-server | awk 'NR==2{print $1}')
+doctl compute reserved-ip-action assign 159.65.208.124 $ID_SERVER
+sleep 10s
+## Deploy server
+echo "Deploy server ..."
+ansible-playbook -i ./ansible/hosts.ini ./ansible/configure_server.yml
+
+# Database
+
+## Create debian-server
+echo "Creating database ..."
+doctl compute droplet create debian-database --droplet-agent=true --enable-monitoring --image ubuntu-20-04-x64 --no-header --region LON1 --size s-2vcpu-2gb --ssh-keys de:8e:e4:13:b9:11:56:dd:05:f1:ce:1c:87:82:de:f7 --tag-name database --wait
+## Assign reserved-ip to database
+echo "Assign reserved ip to database ..."
+ID_DATABASE=$(doctl compute droplet get debian-database | awk 'NR==2{print $1}')
+doctl compute reserved-ip-action assign 159.65.211.183 $ID_DATABASE
+sleep 10s
+## Deploy database
+echo "Deploy database ..."
+ansible-playbook -i ./ansible/hosts.ini ./ansible/configure_database.yml
